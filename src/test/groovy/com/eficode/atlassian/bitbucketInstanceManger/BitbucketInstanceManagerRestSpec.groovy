ed31c0e4a7efda66edf450f53a47083796029fd6
Anders Lantz <lantzanders@gmail.com>
2022-09-15 09:46:21 +0200

    Maven Compile and tests semi works, looking in to compiling for groovy 2 and 3

--- src/test/groovy/com/eficode/atlassian/bitbucketInstanceManger/BitbucketInstanceManagerRestSpec.groovy
+++ src/test/groovy/com/eficode/atlassian/bitbucketInstanceManger/BitbucketInstanceManagerRestSpec.groovy
@@ -4,11 +4,30 @@ import com.eficode.atlassian.bitbucketInstanceManager.BitbucketInstanceManagerRe
 import spock.lang.Shared
 import spock.lang.Specification
 
+/**
+ * Presumes that the Bitbucket instance at baseUrl
+ *      has been started for the first time ever
+ *      bitbucketLicensePath points to a file containing a Bitbucket license
+ *      baseUrl points to the url (and port) of bitbucket
+ *
+ *  Example script using devStack: https://github.com/eficode/devStack
+ *
+ *
+ *
+ import com.eficode.devstack.container.impl.BitbucketContainer
+ BitbucketContainer bitbucketContainer = new BitbucketContainer(dockerHost, dockerCertPath)
+ bitbucketContainer.stopAndRemoveContainer()
+ bitbucketContainer.createContainer()
+ bitbucketContainer.startContainer()
+ */
 
 class BitbucketInstanceManagerRestSpec extends Specification {
 
     @Shared
     static String baseUrl = "http://bitbucket.domain.se:7990"
+
+    @Shared
+    static String bitbucketLicensePath = "src/test/testResources/bitbucket/licenses/bitbucketLicense"
 
     @Shared
     static String restAdmin = "admin"
@@ -23,7 +42,7 @@ class BitbucketInstanceManagerRestSpec extends Specification {
     }
 
     String getBitbucketLicense() {
-        return new File("tests/testResources/bitbucket/licenses/bitbucketLicense").text
+        return new File(bitbucketLicensePath).text
     }
 
     def testSetupOfBase() {
